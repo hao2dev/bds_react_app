4ba9160d176e7746d62522284385591804f9a823
anhdev2k1 <info.anhdev@gmail.com>
2023-02-04 10:25:06 +0700

    delete product

--- src/pages/Products/ProductManager/index.js
+++ src/pages/Products/ProductManager/index.js
@@ -1,4 +1,10 @@
-import React, { Component, useEffect } from "react";
+import React, {
+  Component,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import "../ProductManager/ProductManager.css";
 import Header from "../../../component/Header";
 import FooterSection from "../../../component/FooterSection";
@@ -6,64 +12,63 @@ import abroad from "../../../assets/img/index/abroad-classic01.jpg";
 import advice from "../../../assets/img/index/abroad-unique02.jpg";
 import { useSelector } from "react-redux";
 import { checkLikeProductSelector } from "../../../Redux/selector";
-
+import { Space, Table, Button } from "antd";
+import { deleteDoc, doc } from "@firebase/firestore";
+import { db } from "../../../firebase/connect";
 const ProductLikePage = () => {
+  const [product, setProduct] = useState([]);
   const productLike = JSON.parse(localStorage.getItem("productLike")) || [];
+  const getProductLike = () => {
+    setProduct(productLike);
+  };
+  useEffect(() => {
+    getProductLike();
+  }, []);
+
+  const columns = [
+    {
+      title: "Hình ảnh",
+      dataIndex: "img",
+      key: "img",
+      render: (_, text) => (
+        <Space>
+          <img style={{ width: "150px" }} src={text.img} alt="" />
+        </Space>
+      ),
+    },
+    {
+      title: "Tên",
+      dataIndex: "title",
+      key: "age",
+    },
+    {
+      title: "Giá",
+      dataIndex: "price",
+      key: "address",
+    },
+    {
+      title: "Diện tích",
+      dataIndex: "area",
+      key: "address",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <Button onClick={() => handleDelete(record.id)}>Xóa</Button>
+        </Space>
+      ),
+    },
+  ];
+  const handleDelete = (id) => {
+    const filtered = product.filter((item) => item.id !== id);
+    localStorage.setItem("productLike", JSON.stringify(filtered));
+  };
   return (
     <div>
       <Header />
-      <div className="container content-section">
-        <hr />
-        <h2 className="section-header">
-          <i className="bx bx-building-house" /> Dự án đã thêm
-        </h2>
-        <div className="cart-row">
-          <span className="cart-item cart-header cart-column">DỰ ÁN</span>
-          <span className="cart-price cart-header cart-column">GIÁ</span>
-          <span className="cart-quantity cart-header cart-column">
-            SỐ LƯỢNG
-          </span>
-        </div>
-        <div className="cart-items">
-          {productLike.length > 0 ? (
-            productLike.map((product, index) => {
-              return (
-                <div className="cart-row" key={index}>
-                  <div className="cart-item cart-column">
-                    <img
-                      className="cart-item-image"
-                      alt=""
-                      src={product.img}
-                      width="100px"
-                      height="100px"
-                    />
-                    <span className="cart-item-title">{product.title}</span>
-                  </div>
-                  <span className="cart-price cart-column">
-                    {product.price}
-                  </span>
-                  <div className="cart-quantity cart-column">
-                    <input
-                      className="cart-quantity-input"
-                      type="number"
-                      defaultValue={1}
-                    />
-                    <button className="btn btn-danger" type="button">
-                      Xoá
-                    </button>
-                  </div>
-                </div>
-              );
-            })
-          ) : (
-            <h2>No data !!</h2>
-          )}
-        </div>
-        {/* <div className="cart-total">
-          <strong className="cart-total-title">Tổng</strong>
-          <span className="cart-total-price">3.000.000.000 đ</span>
-        </div> */}
-      </div>
+      <Table columns={columns} dataSource={product} />
       <FooterSection />
     </div>
   );
